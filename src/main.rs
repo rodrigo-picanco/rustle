bbe8d1d1e989c835266e053ff84c6db7c8b66477
Rodrigo Picanço Tavares <rodrigopicancotavares@Rodrigos-MBP.lan>
2023-03-20 18:02:17 +0100

    feat: compare words and output raw data

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-fn run(mut writer: impl std::io::Write){
-    match writeln!(writer, "hello world") {
-        Ok(_) => (),
-        Err(e) => panic!("failed to write to stdout: {}", e),
-    };
+fn compare(a: &str, b: &str) -> Vec<u8> {
+    let mut result = vec![0, 0, 0, 0, 0];
+
+    for (i, c) in b.chars().enumerate() {
+        if c == a.chars().nth(i).unwrap() {
+            result[i] = 2;
+        } else if a.contains(c) {
+            result[i] = 1;
+        }
+    }
+
+    result
 }
 
-fn main() {}
+fn main() {
+    println!("What's the word?");
+
+    let mut word = String::new();
+    std::io::stdin().read_line(&mut word).unwrap();
+
+    let result = compare("hello", &word.trim());
+
+    println!("Result: {:?}", result);
+}
 
 #[test]
-fn it_runs() {
-    let mut result = Vec::new();
-    run(&mut result);
-    assert_eq!(result, b"hello world\n");
+fn it_can_compare_words() {
+    assert_eq!(compare("hello", "hello"), [2, 2, 2, 2, 2]);
+    assert_eq!(compare("hello", "halle"), [2, 0, 2, 2, 1]);
 }
